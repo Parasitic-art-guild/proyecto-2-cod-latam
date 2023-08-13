7efb5259bf9618f7f100a73fa8c3f8de36846fe5
Parasitic-art-guild <plan.mantenimiento.industrial@gmail.com>
2023-08-12 19:35:54 -0500

    patricia-202308121935COP

--- js/script.js
+++ js/script.js
@@ -3,7 +3,8 @@ console.log(menuBtn);
 
 menuBtn.addEventListener
     ('click', function() {
-        document.querySelector("body > header > nav > ul > li > button").classList.toggle('movile-menu-active')
+        document.querySelector("body > header > nav > ul > li > button").classList.toggle('movile-menu-active');
+        document.querySelector("body > header > nav > ul").classList.toggle('movile-menu-active')
     }
 )
 
